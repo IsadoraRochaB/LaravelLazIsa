44825bb81b32658e57084c50182edf41d70d4c9a
IsadoraRochaB <isadorarochabrito.rb@gmail.com>
2022-06-22 10:17:21 -0300

    aula do Herokuzinho

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
